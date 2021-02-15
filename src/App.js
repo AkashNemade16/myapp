061974a73f98f110be1c8098572f195287d0e977
Akash Nemade <37910311+sskkyy962@users.noreply.github.com>
2021-02-14 22:36:43 -0800

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Sidebar from './components/SideBar';
 function App() {
   return (
    <div className='App'>
       <h1>hello</h1>
+       <Sidebar/>
    </div>
   );
 }
