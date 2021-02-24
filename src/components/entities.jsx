8fccd517de8a08977a48e57655340debb4c555d2
Akash Nemade <37910311+sskkyy962@users.noreply.github.com>
2021-02-23 16:00:10 -0800

    entites formatting done

--- src/components/entities.jsx
+++ src/components/entities.jsx
@@ -1,23 +1,64 @@
 import React from 'react';
+import {Grid, makeStyles, Paper} from '@material-ui/core';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        background:"lightgray"
+    },
+}))
 
 const Entities = props => {
-    const data = props.receiveData;
-    console.log(data);
-    return (
-        <div>
-            {data.data?
-                <div>
+        const classes = useStyles();
+        const data = props.receiveData;
+        console.log(data);
+        return (
+            <div className={classes.root}>
+                {data.data ?<div>
                     <h1>Entities</h1>
-                    <h3>Lemmas</h3>
-                    {data.data.entities.map((item,id)=>
-                        (<div key={id}>{item.lemma}</div>))}
-                </div>
+                        <Grid container spacing={3} justify="center"
+                              alignItems="flex-start" direction="row">
+                            <Grid item>
+                                <Paper className={classes.paper}>
+                                    <div>
+                                        <h3>Lemmas</h3>
+                                        {data.data.entities.map((item, id) =>
+                                            (<div key={id}>{item.lemma}</div>))}
+                                    </div>
 
-            :null}
-        </div>
-    );
-};
+                                </Paper>
+                            </Grid>
+                            <Grid item>
+                                <Paper className={classes.paper}>
+                                    <div>
+                                        <h3>Relevance</h3>
+                                        {data.data.entities.map((item, id) =>
+                                            (<div key={id}>{item.relevance}</div>))}
+                                    </div>
+                                </Paper>
+                            </Grid>
+                            <Grid item>
+                                <Paper className={classes.paper}>
+                                    <div>
+                                        <h3>Type</h3>
+                                        {data.data.entities.map((item, id) =>
+                                            (<div key={id}>{item.type}</div>))}
+                                    </div>
+                                </Paper>
+                            </Grid>
+
+
+                        </Grid>
+                    </div>
+                     : null}
+            </div>
+        );
+    }
+;
 
 
 export default Entities;
