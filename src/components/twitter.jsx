302f1e03ffab8f4fb7a38f03ebbec0772b2accc7
Akash Nemade <37910311+sskkyy962@users.noreply.github.com>
2021-06-12 23:56:30 -0700

    server

--- src/components/twitter.jsx
+++ src/components/twitter.jsx
@@ -3,25 +3,27 @@ import { Button, TextField, FormControl } from "@material-ui/core";
 
 const TwitterInput = (props) => {
     const[State,setState] = useState();
-    var Twit  =  require('twitter');
+    var Twitter  =  require('twitter');
+    require('dotenv/config');
 
-    const apiKey = "JDPkzQnyd7w7Qx5UaN7RTZBSW";
-    const apisecretKey = "W9VAzxvfImMaev92CIpqETqj89hhqmCOrleytsMrucbWatLa6I";
-    const accessToken = "1365421072087019521-qVpOkEATWp1RMghfSiBwSgN7tV5pxP";
-    const secretaccessToken = "X4lVMiZ04qtfFG8aBsouWT61PUUvFu861ll2wWbw6SKRm";
+    const apiKey = process.env.apiKey;
+    const apisecretKey = process.env.apisecretKey;
+    const accessToken = process.env.accessToken;
+    const secretaccessToken = process.env.secretAccessToken;
 
-    var T = new Twit(
+
+    var T = new Twitter(
         {
             consumer_key:apiKey,
             consumer_secret:apisecretKey,
             access_token:accessToken,
-            access_token_secret:secretaccessToken
+            access_token_secret:secretaccessToken,
+
+
         }
     )
     const load = () => {
-        T.get('search/tweets', { q: 'moderna since:2020-07-11', count: 100 }, function(err, data, response) {
-            setState(data);
-        })
+       
     }
     // T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
     // console.log(data)
